23add3a3b25f0fc7c958206282c9e8c0c0dd0b03
Yellowtoast <toast@onuii.com>
2023-08-01 21:23:23 +0900

    feat: :sparkles: LoginView의 레이아웃 수정

--- lib/features/authentication/presentation/login/login_view.dart
+++ lib/features/authentication/presentation/login/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pets_next_door_flutter/constants/app_sizes.dart';
+import 'package:pets_next_door_flutter/constants/strings.dart';
 import 'package:pets_next_door_flutter/constants/svgs.dart';
 
 class LoginView extends StatelessWidget {
@@ -9,34 +10,43 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                SvgPicture.asset(Svgs.mainIcon),
-                gapH20,
-                SvgPicture.asset(Svgs.mainTitleIcon),
-              ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SvgPicture.asset(Svgs.mainIcon),
+                  gapH20,
+                  SvgPicture.asset(Svgs.mainTitleIcon),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * (155 / 844),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SvgPicture.asset(Svgs.google),
-                gapW24,
-                SvgPicture.asset(Svgs.kakao),
-                gapW24,
-                SvgPicture.asset(Svgs.apple),
-              ],
+            SizedBox(
+              height: MediaQuery.of(context).size.height * (155 / 844),
             ),
-          ),
-        ],
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset(Svgs.google),
+                      gapW24,
+                      SvgPicture.asset(Svgs.kakao),
+                      gapW24,
+                      SvgPicture.asset(Svgs.apple),
+                    ],
+                  ),
+                  gapH32,
+                  Text(Strings.problemWhenLogin)
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
