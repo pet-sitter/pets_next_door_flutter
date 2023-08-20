1679862593352cf5fb8e5495227d9e04261e2ec7
jihye <kjh9519@naver.com>
2023-08-20 12:13:32 +0900

    fix: :wrench: enum 에 SnsProviderType 추가

--- lib/src/constants/enums.dart
+++ lib/src/constants/enums.dart
@@ -1,1 +1,14 @@
-enum ProviderType { kakao, google, apple }
+enum SnsProviderType {
+  kakao(providerId: 'kakao.com'),
+  google(providerId: 'google.com'),
+  apple(providerId: 'apple.com');
+
+  const SnsProviderType({required this.providerId});
+
+  factory SnsProviderType.getByProviderId({required String providerId}) {
+    return SnsProviderType.values
+        .firstWhere((element) => element.providerId == providerId);
+  }
+
+  final String providerId;
+}
